e86bcb5432d959c2548af0b7027ed715ad46ee0f
Mike Tracz <51129014+freshcup@users.noreply.github.com>
2020-09-11 11:44:09 -0400

    jsx

---
+++ public/scripts/app.js
@@ -0,0 +1,8 @@
+console.log('app.js is running');
+
+// JSX - Javascript XML
+var template = <p>This is JSX from app JSX! </p>;
+var appRoot = document.getElementById('app');
+
+
+ReactDom.render(template, appRoot);
